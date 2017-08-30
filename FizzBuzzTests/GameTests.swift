31a58ad0269b5995e3b9eed265143dede0e2e37b
Rodrigo López-Romero Guijarro <rlromero@gmv.com>
2017-08-30 19:24:51 +0200

    Add new tests file for the game

---
+++ FizzBuzzTests/GameTests.swift
@@ -0,0 +1,21 @@
+//
+//  GameTests.swift
+//  FizzBuzz
+//
+//  Created by Rodrigo López-Romero Guijarro on 30/08/2017.
+//  Copyright © 2017 Rodrigo López-Romero Guijarro. All rights reserved.
+//
+
+import XCTest
+
+class GameTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+}
